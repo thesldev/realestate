b63b63908be3efe27118ac804792384c800dcc39
S.M.D.Sanka Siriwardhana <siriwardhanad.sanka@gmail.com>
2024-02-05 12:20:24 +0530

    setup admin dashboard template

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,13 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+//admin middlewear
+Route::middleware(['auth','role:admin'])->group(function(){
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+//agent middlewear
+Route::middleware(['auth','role:agent'])->group(function(){
+    Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+});
+
